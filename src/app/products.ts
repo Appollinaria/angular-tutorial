3dbc1a27b6f9680b602ead1de5ce0c4921f31352
pomel <porubt@taltech.ee>
2020-12-10 14:18:18 +0200

    Added src and other missing files

---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    id: 0,
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    id: 1,
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    id: 2,
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
